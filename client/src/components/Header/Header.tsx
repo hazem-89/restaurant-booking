36f801c270a9507f0ec38e99b7b9712aab814590
Hazem Kawas <hazem.k.4488@gmail.com>
2022-09-12 14:21:01 +0200

    header component

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,10 +1,57 @@
+import { Box, SxProps, Typography } from '@mui/material'
 
-import React from 'react'
+
+//logo
+import logo from '../../assets/img/logo2.jpg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Box sx={mainBox}>
+      <Box sx={logoBox}>
+        <img src={logo} alt="logo" style={{ width: '50px', height: '50px', marginLeft: '1em', }} />
+        <Typography sx={{ color: '#f1f1f1', fontSize: '2em', textAlign: 'center', paddingLeft: '.5em' }}>
+          Mosaic
+        </Typography>
+      </Box>
+      <Box sx={navText}>
+        <Typography>
+          Home
+        </Typography>
+        <Typography>
+          Reservation
+        </Typography>
+        <Typography>
+          About
+        </Typography>
+      </Box>
+    </Box>
   )
 }
+
+const mainBox: SxProps = {
+  height: '100px',
+  width: '100%',
+  backgroundColor: '#022F3E',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+};
+
+const logoBox: SxProps = {
+  height: '100px',
+  width: '200px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+const navText: SxProps = {
+  width: '300px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  color: '#f1f1f1',
+  marginRight: '4em'
+};
 
 export default Header
