b838f4067f7c92ae1e0e46a51d6e448a7d2a9c1e
Hazem Kawas <hazem.k.4488@gmail.com>
2022-09-12 14:21:10 +0200

    main page hero component

---
+++ client/src/components/MainPageHero/MainPageHero.tsx
@@ -0,0 +1,58 @@
+import { Box, Button, SxProps, Typography } from '@mui/material'
+import MainBackGroundImg from '../../assets/img/123.jpg'
+
+const MainPageHero = () => {
+  return (
+    <Box sx={maineBox}>
+      <Box>
+        <img src={MainBackGroundImg} alt="" style={{ width: '100%', height: '700px', objectFit: 'cover' }} />
+      </Box>
+      <Box sx={mainText}>
+        <Typography sx={{ fontSize: '4em' }}>
+          Mosaic
+        </Typography>
+        <Typography>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos vitae tempora iusto similique aperiam officia non molestias mollitia recusandae. Ullam.
+        </Typography>
+      </Box>
+      <Button sx={reservationBtn}>
+        Reservation
+      </Button>
+    </Box>
+  )
+}
+const maineBox: SxProps = {
+  position: 'relative',
+  height: "700px"
+};
+const mainText: SxProps = {
+  width: '900px',
+  height: '280px',
+  backgroundColor: "rgba(55, 130, 135, 0.36)",
+  color: '#f1f1f1',
+  position: 'absolute',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  top: '50%',
+  textAlign: 'center',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column'
+};
+const reservationBtn: SxProps = {
+  width: '200px',
+  height: '75px',
+  position: 'absolute',
+  right: '4em',
+  bottom: '3em',
+  color: '#f1f1f1',
+  fontWeight: 'bold',
+  fontSize: '1.2em',
+  backgroundColor: 'rgba(35, 63, 65, 0.80)',
+  "& :hover": {
+    backgroundColor: 'black',
+  },
+
+};
+export default MainPageHero
