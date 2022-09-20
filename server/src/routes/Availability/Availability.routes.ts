d2f400630127d7d3e5b42c752fbb6f85cfdf4968
Hazem Kawas <hazem.k.4488@gmail.com>
2022-09-20 21:25:28 +0200

    update packages

--- server/src/routes/Availability/Availability.routes.ts
+++ server/src/routes/Availability/Availability.routes.ts
@@ -6,25 +6,33 @@ const availabilityRouter = Router();
 
 
 availabilityRouter.post('/availability', async (req: Request, res: Response, next: NextFunction) => {
-  const allBookings = await BookingModels.find({ date: req.body.date })
-  const allBookings1 = allBookings.map((booking) => ({ tableId: booking.tableId, time: booking.time.getHours() }))
-  console.log("allBookings", allBookings);
-  const allTables = await TableModels.find()
+  let date = req.body.date
+  if (date) {
+    const allBookings = await BookingModels.find({ date: req.body.date })
+    const allBookings1 = allBookings.map((booking) => ({ tableId: booking.tableId, bookedTime: booking.time }))
+    console.log(allBookings1);
+
+    const allTables = await TableModels.find()
 
 
-  const availableTables = allTables.reduce((prev: any[], current) => {
-    const isAvailableAt18 = !allBookings1.find((booking) => booking.time === 18 && booking.tableId === current.id)
-    const isAvailableAt21 = !allBookings1.find((booking) => booking.time === 21 && booking.tableId === current.id)
-    if (isAvailableAt18 || isAvailableAt21) {
-      prev.push({ tableId: current.id, tableName: current.name })
+    const availableTables = allTables.reduce((prev: any[], current) => {
+      const isAvailableAt18 = !allBookings1.find((booking) => booking.bookedTime === "18:00" && booking.tableId === current.id)
+      const isAvailableAt21 = !allBookings1.find((booking) => booking.bookedTime === "21:00" && booking.tableId === current.id)
+      if (isAvailableAt18 || isAvailableAt21) {
+        prev.push({ tableId: current.id, tableName: current.name })
 
-      prev[prev.length - 1].isAvailableAt18 = isAvailableAt18
-      prev[prev.length - 1].isAvailableAt21 = isAvailableAt21
+        prev[prev.length - 1].isAvailableAt18 = isAvailableAt18
+        prev[prev.length - 1].isAvailableAt21 = isAvailableAt21
 
-    }
-    return prev
-  }, [])
-  res.status(200).send({ availableTables })
+      }
+      return prev
+    }, [])
+    res.status(200).send({ availableTables })
+    console.log(req.body.date);
+  } else {
+    console.log('no req.body.date');
+  }
+
 })
 
 
