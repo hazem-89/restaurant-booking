676f6ac69f2b19533e5ef935a13146187444ad20
Hazem Kawas <hazem.k.4488@gmail.com>
2022-09-21 15:10:06 +0200

    add confirmation page

--- client/src/pages/Confirmation/ConfirmationPage.tsx
+++ client/src/pages/Confirmation/ConfirmationPage.tsx
@@ -1,9 +1,37 @@
+import { Box, SxProps, Typography } from '@mui/material'
 import React from 'react'
+import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
 
 const ConfirmationPage = () => {
   return (
-    <div style={{ color: 'white' }}>ConfirmationPage</div>
+    <Box style={{ color: 'white' }}>
+      <Box>
+        <Header />
+      </Box>
+      <Box sx={mainBox}>
+        <Typography sx={{ fontSize: '2em' }}>
+          You reservation is done
+        </Typography>
+        <Typography>
+          Thanks for choosing us
+        </Typography>
+      </Box>
+
+      <Box>
+        <Footer />
+      </Box>
+    </Box>
   )
 }
 
-export default ConfirmationPage 
+export default ConfirmationPage
+
+
+const mainBox: SxProps = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: { xs: '100px', md: '', lg: '500px' }
+}
