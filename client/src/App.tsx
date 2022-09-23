0899a5f0d93b49f9a014f91eda8a213c8c41f04f
Hazem Kawas <hazem.k.4488@gmail.com>
2022-09-23 17:43:55 +0200

    add admin page rout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,14 @@ import HomePage from './pages/HomePage/HomePage';
 import Reservation from './pages/Reservation/Reservation';
 import ContactPage from './pages/ContactPage/Contact';
 import './App.css';
+import AminPage from './pages/AminPage/AminPage';
 function App() {
   return (
     <Routes >
       <Route path="/" element={<HomePage />} />
       <Route path="/reservation" element={<Reservation />} />
       <Route path="/confirmation" element={<ConfirmationPage />} />
+      <Route path="/admin" element={<AminPage />} />
       <Route path="/contact" element={<ContactPage />} />
 
       {/* <Route path="*" element={< />} /> */}
